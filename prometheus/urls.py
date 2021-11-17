4d30539c771d6b9f399d061f65f5db6bca5a4a97
“iimnurd” <iim@alterra.id>
2021-11-17 18:46:15 +0700

    modify prom client

--- prometheus/urls.py
+++ prometheus/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import homePageView, metricsPageView, ovoPageView, gopayPageView
 
 urlpatterns = [
+    path('', homePageView, name='home'),
     path('ovo', ovoPageView, name='ovo'),
     path('gopay', gopayPageView, name='gopay'),
     path('metrics', metricsPageView, name='metrics')
